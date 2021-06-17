0855d34a1060c51592f62aad369f78f0a5de3b64
Edan Schwartz <edanschwartz@gmail.com>
2021-06-17 12:52:39 -0500

    Fixed missing operator

--- assignment/helloWorld.js
+++ assignment/helloWorld.js
@@ -2,5 +2,5 @@ console.log('Hello to Mersenne!!!!');
 
 let bigNumber = 537829217;
 let littleNumber = 12;
-let weirdNumber = littleNumber  bigNumber;
+let weirdNumber = littleNumber / bigNumber;
 console.log('weird number is', weirdNumber);
