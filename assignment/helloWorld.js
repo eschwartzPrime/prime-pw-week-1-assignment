1824b01d23be3a801ce992f8ea3db84cbf7b3e9e
Edan Schwartz <edanschwartz@gmail.com>
2021-06-17 12:50:25 -0500

    totally didn't break anything

--- assignment/helloWorld.js
+++ assignment/helloWorld.js
@@ -1,6 +1,6 @@
-console.log('Hello Mersenne!!!!');
+console.log('Hello to Mersenne!!!!');
 
 let bigNumber = 537829217;
 let littleNumber = 12;
-let weirdNumber = littleNumber / bigNumber;
+let weirdNumber = littleNumber  bigNumber;
 console.log('weird number is', weirdNumber);
